c0ec67ed9e2a4a1c309abf988613313b5b5404bb
Vincent <dakinnie@gmail.com>
2021-08-02 18:39:05 +0300

    Testing data fetching

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,17 +1,34 @@
+import React, { useState, useEffect } from "react";
 import "./Search.css";
 import Input from "../Input/Input";
 import axios from "axios";
 
 const SearchBar = () => {
-  let city = "Mwanza";
+  const [myCity, setCity] = useState("Mwanza");
   const API_KEY = `${process.env.REACT_APP_OWM_API_KEY}`;
-  axios
-    .get(
-      `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`
-    )
-    .then((response) => {
-      console.log(response.data);
-    });
+  const [weatherData, setWeather] = useState([]);
+
+  useEffect(() => {
+    const getUserCity = () => {
+      let userInput = prompt("Enter a City");
+      setCity(userInput);
+    };
+    getUserCity();
+  }, []);
+
+  useEffect(() => {
+    const fetchWeatherData = (city) => {
+      axios
+        .get(
+          `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`
+        )
+        .then((response) => {
+          setWeather(response.data);
+        });
+    };
+    fetchWeatherData(myCity);
+  }, [API_KEY, myCity]);
+  console.log(weatherData);
 
   return (
     <div className="container">
